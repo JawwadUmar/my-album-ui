d2f6d0439de2c2222d6eaefb7f5d1a553dc96e94
Jawwad Umar <jawwadumar99@gmail.com>
2026-02-05 01:52:37 +0530

    date added

--- src/utils/helpFunctions.ts
+++ src/utils/helpFunctions.ts
@@ -1,3 +1,35 @@
-export const getImageUrl = (key: string, S3_BASE_URL:string) => {
+import { type Photo } from "../api/auth";
+
+export const getImageUrl = (key: string, S3_BASE_URL: string) => {
     return `${S3_BASE_URL}${key}`;
+};
+
+export const groupPhotosByMonth = (photos: Photo[]): Record<string, Photo[]> => {
+    return photos.reduce((groups, photo: Photo) => {
+        let key = "Unknown Date";
+
+        // Handle inconsistent casing from API (created_at vs CreatedAt)
+        // console.log(photo);
+
+        const dateStr: string = photo.created_at;
+        if (dateStr) {
+            const cleanDateStr = String(dateStr).replace(" ", "T");
+            const date = new Date(cleanDateStr);
+
+            // Check if date is valid
+            if (!isNaN(date.getTime())) {
+                key = date.toLocaleDateString("en-US", {
+                    month: "long",
+                    year: "numeric",
+                });
+            }
+        }
+
+        if (!groups[key]) {
+            groups[key] = [];
+        }
+
+        groups[key].push(photo);
+        return groups;
+    }, {} as Record<string, Photo[]>);
 };
